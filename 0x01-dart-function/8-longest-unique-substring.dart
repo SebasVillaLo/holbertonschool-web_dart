b0b1df1caca8bb34d2be258670abbda29b183c2d
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-03-16 15:48:52 -0500

    Update 8-longest-unique-substring.dart

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,3 +1,9 @@
 String longestUniqueSubstring(String str) {
-  return str.split('').toSet().join();
+  var maxLength = 0, start = 0, charIndex = {};
+  str.split('').asMap().forEach((i, char) {
+    start = charIndex.containsKey(char) && charIndex[char] >= start ? charIndex[char] + 1 : start;
+    charIndex[char] = i;
+    maxLength = maxLength > i - start + 1 ? maxLength : i - start + 1;
+  });
+  return str.substring(start, start + maxLength);
 }
